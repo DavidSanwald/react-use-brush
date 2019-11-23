662c2423e0e27599a73256c541c6d074d4049c53
DavidSanwald <david.sanwald@gmail.com>
2019-11-23 11:58:26 +0100

    update example for dragging

--- example/index.tsx
+++ example/index.tsx
@@ -1,10 +1,11 @@
 import 'react-app-polyfill/ie11';
 
-import { scaleLinear } from 'd3-scale';
+import { scaleLinear, scaleSqrt } from 'd3-scale';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 import { inBounds, useBrush } from '../.';
+import { Bubble } from './Bubble';
 import { bubbleData as data } from './data';
 
 const size = { width: 900, height: 600 };
@@ -22,26 +23,31 @@ const App = () => {
   const yScale = scaleLinear()
     .range([height, 0])
     .domain(extent(data.map(d => d.y)));
-  const [state, rect, bind, selection] = useBrush();
+  const rScale = scaleSqrt()
+    .range([5, 40])
+    .domain(extent(data.map(d => d.z)));
+  const [state, rect, rectRef, bind, selection] = useBrush();
 
   return (
-    <svg {...bind} width={width} height={height}>
-      <rect {...rect} fill="none" stroke="black" />
-      {data.map(({ x, y, name }) => {
-        const cx = xScale(x);
-        const cy = yScale(y);
-        const inside = inBounds(selection)([cx, cy]);
-        return (
-          <circle
-            cx={cx}
-            cy={cy}
-            r="5"
-            fill={inside ? 'red' : 'blue'}
-            key={name}
-          />
-        );
-      })}
-    </svg>
+    <>
+      <svg {...bind} width={width} height={height}>
+        <rect
+          {...rect}
+          ref={rectRef}
+          fill="none"
+          stroke="black"
+          pointerEvents="all"
+        />
+        {data.map(({ x, y, z, name }) => {
+          const cx = xScale(x);
+          const cy = yScale(y);
+          const r = rScale(z);
+          const inside = inBounds(selection)([cx, cy]);
+          return <Bubble x={cx} y={cy} r={r * (inside ? 2 : 1)} key={name} />;
+        })}
+      </svg>
+      <pre>{JSON.stringify(state)}</pre>
+    </>
   );
 };
 
