b2ff8f4f130c887ea1e6c637950ebf5369639234
David Sanwald <DavidSanwald@users.noreply.github.com>
2019-12-01 14:05:35 +0100

    test: add more tests for dagging and non dragging (#31)

--- test/useBrush.test.ts
+++ test/useBrush.test.ts
@@ -1,6 +1,108 @@
 import { renderHook } from '@testing-library/react-hooks';
 
-import { Action, Brush, reducer, useBrush } from '../src';
+import { Action, Brush, dragReducer, reducer, useBrush } from '../src';
+
+describe('the dragReducer', () => {
+  it('switches to starting state from being closed, on mousedown action', () => {
+    const initialState: Brush = {
+      start: [0, 0],
+      current: [0, 0],
+      status: 'CLOSED',
+      selection: { start: [0, 0], current: [0, 0] },
+    };
+    const nextState: Brush = {
+      start: [1, 1],
+      current: [1, 1],
+      status: 'BRUSH_START',
+    };
+
+    const action: Action = {
+      type: 'MOUSE_DOWN',
+      payload: [1, 1],
+    };
+    const result = dragReducer(initialState, action);
+    expect(result).toEqual(nextState);
+  });
+  it("switches to brushing status from 'BRUSH_START' status, when the mouse is moved", () => {
+    const initialState: Brush = {
+      start: [0, 0],
+      current: [0, 0],
+      status: 'BRUSH_START',
+    };
+
+    const action: Action = {
+      type: 'MOUSE_MOVE',
+      payload: [1, 1],
+    };
+
+    const nextState: Brush = {
+      start: [0, 0],
+      current: [1, 1],
+      status: 'BRUSHING',
+    };
+    const result = dragReducer(initialState, action);
+    expect(result).toEqual(nextState);
+  });
+  it('if mouse up event occures during brushing the brush closes but saves the selection', () => {
+    const initialState: Brush = {
+      start: [1, 1],
+      current: [2, 2],
+      status: 'BRUSHING',
+    };
+
+    const action: Action = {
+      type: 'MOUSE_UP',
+    };
+
+    const nextState: Brush = {
+      start: [1, 1],
+      current: [2, 2],
+      status: 'BRUSH_END',
+    };
+    const result = dragReducer(initialState, action);
+    expect(result).toEqual(nextState);
+  });
+
+  it('if mouse up event occures during brushing the brush closes but saves the selection', () => {
+    const initialState: Brush = {
+      start: [1, 1],
+      current: [2, 2],
+      status: 'BRUSH_START',
+    };
+
+    const action: Action = {
+      type: 'MOUSE_UP',
+    };
+
+    const nextState: Brush = {
+      start: [1, 1],
+      current: [2, 2],
+      status: 'BRUSH_END',
+    };
+    const result = dragReducer(initialState, action);
+    expect(result).toEqual(nextState);
+  });
+
+  it('if mouse up event occures during brushing the brush closes but saves the selection', () => {
+    const initialState: Brush = {
+      start: [1, 1],
+      current: [2, 2],
+      status: 'BRUSH_START',
+    };
+
+    const action: Action = {
+      type: 'MOUSE_UP',
+    };
+
+    const nextState: Brush = {
+      start: [1, 1],
+      current: [2, 2],
+      status: 'BRUSH_END',
+    };
+    const result = dragReducer(initialState, action);
+    expect(result).toEqual(nextState);
+  });
+});
 
 describe('the reducer', () => {
   it('switches to starting state from being closed, on mousedown action', () => {
